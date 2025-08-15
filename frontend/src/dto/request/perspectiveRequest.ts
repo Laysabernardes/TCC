9bd5d8ada32e95b5641e35c734f1ea1a3479e3c0
LucasLoopsT <128094146+LucasLoopsT@users.noreply.github.com>
2025-08-15 17:18:35 -0300

    feat: perspective form component

---
+++ frontend/src/dto/request/perspectiveRequest.ts
@@ -0,0 +1,16 @@
+export default interface PerspectiveRequest {
+  _id: string;
+  projectId: string;
+  perspective_title: string;
+  perspective_subtitle: string;
+  perspective_slug: string;
+  perspective_order: number;
+  perspective_about_html: string;
+  perspective_refs: string[];
+  perspective_team: any[];
+  perspective_status: string;
+  perspective_isCarrossel: boolean;
+  perspective_orderCarrossel: number;
+  perspective_banner: string;
+  perspective_extraURL: string;
+}
